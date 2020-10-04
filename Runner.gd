39cbd9070095d359821043af30e4d19fbc68e1ba
Isabella Boone <ib2573@cs.ship.edu>
2020-10-04 16:43:13 -0400

    Player movement

--- Runner.gd
+++ Runner.gd
@@ -1,15 +1,14 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var player = preload("res://Player.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#Center Window
 	OS.set_window_position(OS.get_screen_size(0) * 0.5 - OS.get_window_size() * 0.5)
+	var player = player.instance()
+	add_child(player)
+	
 	pass # Replace with function body.
 
 

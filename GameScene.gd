d170d216f4dea3cdb6d705f7571e965c3c63bc24
Steven Hetrick <stevenhetrick@msn.com>
2020-10-04 22:02:47 -0400

    border and file cleanup

--- GameScene.gd
+++ GameScene.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
-var BackgroundTile_scene = preload("res://BackgroundTiles.tscn")
-var platform_scene = preload("res://Platform.tscn")
+var border_scene = preload("res://Map/Border.tscn")
+var backgroundtile_scene = preload("res://Map/BackgroundTiles.tscn")
+var platform_scene = preload("res://Map/Platform.tscn")
 var player_scene = preload("res://Player.tscn")
 
 # Called when the node enters the scene tree for the first time.
@@ -9,9 +10,12 @@ func _ready():
 	
 	for i in range(11):
 		for j in range(19):
-			var tile = BackgroundTile_scene.instance()
+			var tile = backgroundtile_scene.instance()
 			tile.position = Vector2(96*i+48, 32*j+16)
 			add_child(tile)	
+	
+	var border = border_scene.instance()
+	add_child(border)
 	
 	var platform = platform_scene.instance()
 	#platform.position = Vector2(get_viewport().size.x/2, get_viewport().size.y/2)

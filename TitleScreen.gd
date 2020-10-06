b8011230ebba345651c9635477336b3f29133097
Isabella <ib2573@cs.ship.edu>
2020-10-06 13:58:30 -0400

    Added background music to title screen

--- TitleScreen.gd
+++ TitleScreen.gd
@@ -4,9 +4,15 @@ extends Control
 func _on_PlayButton_pressed():
 	print("Play")
 	get_tree().change_scene("res://GameScene.tscn")
+	if($BackgroundMusic.playing == true):
+		$BackgroundMusic.playing = false
 	
 func _on_HowToPlayButton_pressed():
 	print("How to play")
 	
 func _on_ExitButton_pressed():
 	print("Exit")
+
+func _ready(): 
+	$BackgroundMusic.volume_db = 1
+	$BackgroundMusic.play()
